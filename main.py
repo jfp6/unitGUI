93752a2e841a16d52c75b54e82e03f8ff1b38fa8
Joseph Prince <stonewahl@gmail.com>
2022-06-25 14:44:16 -0600

    add tonne

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.header("Mass SI")
-    mass_kg = st.number_input("Enter kg")*ur("kg")
-    st.write('{:.1f}'.format(mass_kg.to("lb")))
+    mass_kg = st.number_input("Enter kg",step=1.0)*ur("kg")
+    st.write('{:.2f}'.format(mass_kg.to("lb")))
+    st.write('{:.4f}'.format(mass_kg.to("tonne"))
+    st.header("Length SI")
+    
 
 with col2:
     st.header("Mass USCS")
-    mass_lbm = st.number_input("Enter lbm")*ur("lb")
-    st.write('{:.1f}'.format(mass_lbm.to("kg")))
+    mass_lbm = st.number_input("Enter lbm",step=1.0)*ur("lb")
+    st.write('{:.2f}'.format(mass_lbm.to("kg")))
+    st.write('{:.4f}'.format(mass_lbm.to("tonne")))
+    st.header("Length USCS")
     
