d40ef351307c4e035d55668d19a65c2905e80844
Joseph Prince <stonewahl@gmail.com>
2022-06-25 16:06:40 -0600

    1 col

--- main.py
+++ main.py
@@ -9,24 +9,15 @@ st.write("# Unit Conversions")
 
 conversion = st.radio("Select Conversion",("Length","Area","Volume","Volumetric Flow","Mass","Mass Flow"),horizontal=True)
 
-col1,col2 = st.columns([3,3])
-
-with col1:
-    if conversion == "Mass":
-        st.header("Mass")
-        mass = st.number_input("Enter kg",step=1.0)
-        st.write('{:.2f}'.format(mass*ur(massu).to("kg")))
-        st.write('{:.2f}'.format(mass*ur(massu).to("lb")))
-        st.write('{:.4f}'.format(mass*ur(massu).to("tonne")))
-    elif conversion == "Length":
-        st.header("Length")
-        length_m = st.number_input("Enter m",step=1.0)*ur("m")
-        st.write('{:.2f}'.format(length_m.to("in")))
-with col2:
-    if conversion == "Mass":
-        st.header("Unit In")
-        massu = st.selectbox("Unit",("kg","tonne","lb","slug"))
-    elif conversion == "Length":
-        st.header("Length")
-        length_m = st.number_input("Enter m",step=1.0)*ur("m")
-        st.write('{:.2f}'.format(length_m.to("in")))
+if conversion == "Mass":
+    st.header("Mass")
+    st.header("Unit In")
+    massu = st.selectbox("Unit",("kg","tonne","lb","slug"))
+    mass = st.number_input("Enter kg",step=1.0)
+    st.write('{:.2f}'.format(mass*ur(massu).to("kg")))
+    st.write('{:.2f}'.format(mass*ur(massu).to("lb")))
+    st.write('{:.4f}'.format(mass*ur(massu).to("tonne")))
+elif conversion == "Length":
+    st.header("Length")
+    length_m = st.number_input("Enter m",step=1.0)*ur("m")
+    st.write('{:.2f}'.format(length_m.to("in")))
