46d8ada5484a847a586aa33f5c1d7a2c6b59d6e5
Joseph Prince <stonewahl@gmail.com>
2022-06-25 16:12:36 -0600

    2 col again

--- main.py
+++ main.py
@@ -9,15 +9,11 @@ st.write("# Unit Conversions")
 
 conversion = st.radio("Select Conversion",("Length","Area","Volume","Volumetric Flow","Mass","Mass Flow"),horizontal=True)
 
+col1, col2 = st.columns([3,2])
+
 if conversion == "Mass":
-    st.header("Mass")
-    st.header("Unit In")
-    massu = st.selectbox("Unit",("kg","tonne","lb","slug"))
-    mass = st.number_input("Enter kg",step=1.0)
-    st.write('{:.2f}'.format(mass*ur(massu).to("kg")))
-    st.write('{:.2f}'.format(mass*ur(massu).to("lb")))
-    st.write('{:.4f}'.format(mass*ur(massu).to("tonne")))
-elif conversion == "Length":
-    st.header("Length")
-    length_m = st.number_input("Enter m",step=1.0)*ur("m")
-    st.write('{:.2f}'.format(length_m.to("in")))
+    massu = col2.selectbox("Unit",("kg","tonne","lb","slug"))
+    mass = col1.number_input("Input",step=1.0)
+    col1.write('{:.2f}'.format(mass*ur(massu).to("kg")))
+    col1.write('{:.2f}'.format(mass*ur(massu).to("lb")))
+    col1.write('{:.4f}'.format(mass*ur(massu).to("tonne")))
