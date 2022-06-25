4150ed0ed67a8c70c6418b140b2a5065a6d2b6d2
Joseph Prince <stonewahl@gmail.com>
2022-06-25 16:31:43 -0600

    add T

--- main.py
+++ main.py
@@ -7,14 +7,29 @@ pi = math.pi
 
 st.write("# Unit Conversions")
 
-conversion = st.radio("Select Conversion",("Pressure","Temperature","Length","Area","Volume","Volumetric Flow","Mass","Mass Flow"),horizontal=True)
+conversion = st.radio("Select Conversion",("Pressure","Temperature","Length","Area","Volume","Volumetric Flow","Mass","Mass Flow","Energy"),horizontal=True)
 
 col1, col2 = st.columns([3,2])
 
-if conversion == "Mass":
-    massu = col2.selectbox("Unit",("kg","tonne","lb","slug"))
-    mass = col1.number_input("Input",step=1.0)
-    col1.write('{:.2f}'.format(mass*ur(massu).to("kg")))
-    col1.write('{:.2f}'.format(mass*ur(massu).to("lb")))
-    col1.write('{:.4f}'.format(mass*ur(massu).to("tonne")))
-    
+if conversion == "Pressure":
+    unit = col2.selectbox("Unit",("Pa","kPa","MPa","bar","psi"))
+    qty = col1.number_input("Input",step=1.0)
+    col1.write('{:.0}'.format(qty*ur(unit).to("Pa")))
+    col1.write('{:.0f}'.format(qty*ur(unit).to("kPa")))
+    col1.write('{:.3f}'.format(qty*ur(unit).to("MPa")))
+    col1.write('{:.2f}'.format(qty*ur(unit).to("bar")))
+    col1.write('{:.1f}'.format(qty*ur(unit).to("psi")))
+elif conversion == "Temperature":
+    unit = col2.selectbox("Unit",("K","C","F","R"))
+    qty = col1.number_input("Input",step=1.0)
+    col1.write('{:.1f}'.format(qty*ur_(unit).to("C")))
+    col1.write('{:.1f}'.format(qty*ur_(unit).to("K")))
+    col1.write('{:.1f}'.format(qty*ur_(unit).to("F")))
+    col1.write('{:.1f}'.format(qty*ur_(unit).to("R")))
+elif conversion == "Mass":
+    unit = col2.selectbox("Unit",("kg","tonne","lb","slug"))
+    qty = col1.number_input("Input",step=1.0)
+    col1.write('{:.2f}'.format(qty*ur(unit).to("kg")))
+    col1.write('{:.4f}'.format(qty*ur(unit).to("tonne")))
+    col1.write('{:.2f}'.format(qty*ur(unit).to("lb")))
+    col1.write('{:.4f}'.format(qty*ur(unit).to("slug")))
