e638a90f8a59ae49d5b73f7ee78b2acae5ac0ac4
Joseph Prince <stonewahl@gmail.com>
2022-06-25 16:41:12 -0600

    test 3 direct

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ col1, col2 = st.columns([3,2])
 if conversion == "Pressure":
     unit = col2.selectbox("Unit",("Pa","kPa","MPa","bar","psi"))
     qty = col1.number_input("Input",step=1.0)
-    col1.write(format(qty*ur(unit).to("Pa"),"~"))
+    col1.write('{:.0f}'.format(qty.to("Pa").magnitude,"Pa")
     col1.write('{:.0f}'.format(qty*ur(unit).to("kPa")))
     col1.write('{:.3f}'.format(qty*ur(unit).to("MPa")))
     col1.write('{:.2f}'.format(qty*ur(unit).to("bar")))
