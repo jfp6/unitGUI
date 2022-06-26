eb98244bc4d7b7ae383ad0e160f8a83ac3a6dcab
Joseph Prince <stonewahl@gmail.com>
2022-06-25 22:18:56 -0600

    change order col1 col2

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ conversion = st.radio("Select Conversion",("Pressure","Temperature","Length","Ar
 col1, col2 = st.columns([3,2])
 
 if conversion == "Pressure":
-    unit = col2.selectbox("Unit",("Pa","kPa","MPa","bar","psi"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("Pa","kPa","MPa","bar","psi"))
     value = qty*ur(unit)
     col1.write('{:.3g}'.format(value.to("Pa").magnitude)+" Pa")
     col1.write('{:.3g}'.format(value.to("kPa").magnitude)+" kPa")
@@ -21,24 +21,24 @@ if conversion == "Pressure":
     col1.write('{:.3g}'.format(value.to("bar").magnitude)+" bar")
     col1.write('{:.3g}'.format(value.to("psi").magnitude)+" psi")
 elif conversion == "Temperature":
-    unit = col2.selectbox("Unit",("K","degC","degF","degR"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("K","degC","degF","degR"))
     value = ur_(qty,unit)
     col1.write('{:.3g}'.format(value.to("K").magnitude)+" K")
     col1.write('{:.3g}'.format(value.to("degC").magnitude)+" degC")
     col1.write('{:.3g}'.format(value.to("degF").magnitude)+" degF")
     col1.write('{:.3g}'.format(value.to("degR").magnitude)+" degR")
 elif conversion == "Length":
-    unit = col2.selectbox("Unit",("mm","m","in","ft"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("mm","m","in","ft"))
     value = qty*ur(unit)
     col1.write('{:.3g}'.format(value.to("mm").magnitude)+" mm")
     col1.write('{:.3g}'.format(value.to("m").magnitude)+" m")
     col1.write('{:.3g}'.format(value.to("in").magnitude)+" in")
     col1.write('{:.3g}'.format(value.to("ft").magnitude)+" ft")
 elif conversion == "Area":
-    unit = col2.selectbox("Unit",("mm2","m2","in2","ft2"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("mm2","m2","in2","ft2"))
     if unit == "mm2":
         unit = "mm**2"
     elif unit == "m2":
@@ -53,8 +53,8 @@ elif conversion == "Area":
     col1.write('{:.3g}'.format(value.to("in**2").magnitude)+" in2")
     col1.write('{:.3g}'.format(value.to("ft**2").magnitude)+" ft2")
 elif conversion == "Volume":
-    unit = col2.selectbox("Unit",("mm3","m3","in3","ft3"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("mm3","m3","in3","ft3"))
     if unit == "mm3":
         unit = "mm**3"
     elif unit == "m3":
@@ -69,8 +69,8 @@ elif conversion == "Volume":
     col1.write('{:.3g}'.format(value.to("in**3").magnitude)+" in3")
     col1.write('{:.3g}'.format(value.to("ft**3").magnitude)+" ft3")
 elif conversion == "Volumetric Flow":
-    unit = col2.selectbox("Unit",("m3/hr","m3/s","gpm","ft3/s"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("m3/hr","m3/s","gpm","ft3/s"))
     if unit == "m3/hr":
         unit = "m**3/hr"
     elif unit == "m3/s":
@@ -85,16 +85,16 @@ elif conversion == "Volumetric Flow":
     col1.write('{:.3g}'.format(value.to("gallon/min").magnitude)+" gpm")
     col1.write('{:.3g}'.format(value.to("ft**3/s").magnitude)+" ft3/s")
 elif conversion == "Mass":
-    unit = col2.selectbox("Unit",("kg","tonne","lb","slug"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("kg","tonne","lb","slug"))
     value = qty*ur(unit)
     col1.write('{:.3g}'.format(value.to("kg").magnitude)+" kg")
     col1.write('{:.3g}'.format(value.to("tonne").magnitude)+" tonne")
     col1.write('{:.3g}'.format(value.to("lb").magnitude)+" lb")
     col1.write('{:.3g}'.format(value.to("slug").magnitude)+" slug")
 elif conversion == "Mass Flow":
-    unit = col2.selectbox("Unit",("kg/hr","tonne/hr","kg/s","lb/hr","lb/s"))
     qty = col1.number_input("Input",step=1.0)
+    unit = col2.selectbox("Unit",("kg/hr","tonne/hr","kg/s","lb/hr","lb/s"))
     value = qty*ur(unit)
     col1.write('{:.3g}'.format(value.to("kg/hr").magnitude)+" kg/hr")
     col1.write('{:.3g}'.format(value.to("tonne/hr").magnitude)+" tonne/hr")
